19973cf3ed2a91b19ee06297c8e7bb84ea61ae75
shainasahu <145936084+shainasahu@users.noreply.github.com>
2025-10-04 14:56:16 -0400

    created nextjs app + backedn folder

---
+++ my-project/app/pages/results.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Results() {
+  return (
+    <div className={styles.page}>
+      hi guys PLANNING
+    </div>
+  );
+}

5f6594465d0bd9e9b22a6652050cd1198bc06ff1
advita-sharma <advita.sharma4@gmail.com>
2025-10-05 06:41:09 -0400

    adding carousel recap

--- my-project/app/components/IphoneFrame.js
+++ my-project/app/components/IphoneFrame.js
@@ -10,11 +10,14 @@ export default function IphoneFrame({ children }) {
           <div className="w-full h-full bg-white rounded-[36px] overflow-y-auto relative">
             {/* Top bezel area (black border frame) */}
             <div className="absolute top-0 left-0 w-full h-[40px] bg-black rounded-t-[36px] z-10" />
-  
-            {/* Actual app content */}
-            <div className="relative z-20 pt-[40px]"> 
-              {children}
-            </div>
+
+              {/* Modal root for app-scoped popups (portal target) */}
+              <div id="iphone-modal-root" className="absolute inset-0 z-30 pointer-events-none" />
+
+              {/* Actual app content */}
+              <div className="relative z-20 pt-[40px]"> 
+                {children}
+              </div>
           </div>
         </div>
       </div>
